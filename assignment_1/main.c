71c2b72218227b52e80a8fa1adaf4c4b11108cfe
haylsagler1999 <hayley.agler@mail.utoronto.ca>
2021-05-05 20:02:16 -0400

    step 10: changed main.c to output the square of the average

--- assignment_1/main.c
+++ assignment_1/main.c
@@ -6,8 +6,10 @@ int main() {
     double arr[] = {1.0, 2.0, 3.0, 4.0};
 
     double result = average(4, arr);
-
-    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    //square the result
+    result = result*result;
+    //change the print statement to say its the square of the average
+    printf("The square of the average of 1, 2, 3 and 4 is: %.4f\n", result);
     return 0;    
 }
 
